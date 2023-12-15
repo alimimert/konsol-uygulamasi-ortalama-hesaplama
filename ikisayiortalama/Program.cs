af7e1cc8d45762f95db8c5e31ac6cd506d8f058f
alimimert <ali.mert2048@gmail.com>
2023-12-15 16:53:31 +0300

    Initial commit

---
+++ ikisayiortalama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        do
+        {
+            Console.WriteLine("İki sayı giriniz:");
+
+            // Kullanıcıdan ilk sayıyı girmesini iste
+            Console.Write("Birinci sayı: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            // Kullanıcıdan ikinci sayıyı girmesini iste
+            Console.Write("İkinci sayı: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            // İki sayının ortalamasını hesapla
+            double ortalama = (sayi1 + sayi2) / 2;
+
+            // Ortalamayı ekrana yazdır
+            Console.WriteLine("Girilen sayıların ortalaması: " + ortalama);
+
+            Console.WriteLine("Başka bir işlem yapmak ister misiniz? (Evet/Hayır): ");
+        } while (Console.ReadLine().Equals("Evet", StringComparison.OrdinalIgnoreCase));
+
+        Console.WriteLine("Program sonlandı. İyi günler!");
+    }
+}
